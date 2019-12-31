deec7813a7d8eecdc0754432b3e6cce100cc079f
McKenzie Gary <mackdgary@gmail.com>
2019-12-31 13:42:42 -0500

    Add files via upload

---
+++ GUI/WeatherGUI/WeatherGUI.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import requests
+
+CANVASHEIGHT = 400
+CANVASWIDTH = 600
+
+def test_function(entry):
+    print("This is the Entry:", entry)
+
+
+#api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+
+def format_response(weather):
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+
+        final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (name, desc, temp)
+    except:
+        final_str = 'There was a problem retrieving that information :('
+
+    return final_str
+
+def get_weather(city):
+    weather_key = '2667caf39670221cd6fbe165f0a22fb8'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    weather = response.json()
+
+    label['text'] = format_response(weather)
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height = CANVASHEIGHT , width = CANVASWIDTH)
+canvas.pack()
+
+
+background_image = tk.PhotoImage(file='landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='blue', bd=5)
+frame.place (relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text = 'Get Weather', width = '20', height = '3', font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='blue', bd=5)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, bg='white', font=40)
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
